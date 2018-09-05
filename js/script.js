67fa05d3facd31c2e6c91d4d0fe47d286daaef50
IrinaRostovtseva <ni8003@yandex.ru>
2018-09-05 23:40:51 +0300

    реализована валидация формы с выводом сообщения; обновлены минифицированные css и script файлы

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 var modalControl = document.querySelector(".modal-operator");
 var popup = document.querySelector(".modal");
 var arriveDate = popup.querySelector("[name=arrival-date]");
+var departureDate = popup.querySelector("[name=departure-date]");
+var adult = popup.querySelector("[name=adult-amount]");
+var children = popup.querySelector("[name=children-amount]");
+var form = document.querySelector("form");
       
-modalControl.addEventListener("click", function(){
-    popup.classList.toggle("modal-open");
-    arriveDate.focus();
+modalControl.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.toggle("modal-open");
+  form.classList.remove("error-message");
+  arriveDate.focus();
+});
+form.addEventListener("submit", function (evt) {
+  if (!arriveDate.value || !departureDate.value || !adult.value || !children.value) {
+    evt.preventDefault();
+    form.classList.remove("error-message");
+    form.offsetWidth = form.offsetWidth;
+    form.classList.add("error-message");
+  }
 });
